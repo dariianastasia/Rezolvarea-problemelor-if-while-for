9baf6529e49f5c4d9211a9799b1a6c5e2688f360
dariianastasia <71002237+dariianastasia@users.noreply.github.com>
2020-11-10 10:21:29 +0300

    Add files via upload

---
+++ pr.1.py
@@ -0,0 +1,10 @@
+n=int(input("introduceti numarul:"))
+
+if n==28 or n==29:
+    print("februarie")
+elif n==30:
+    print("aprilie, iunie, septembrie, noiembrie")
+elif n==31:
+    print("ianuarie ,martie ,mai ,iulie, august, octombrie, decembrie")
+else:
+    print("nu exista asa luna")
